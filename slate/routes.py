de961fea7bbf8cdfbfc024bdbfb9e2dba3e65748
kryptn <kryptn@gmail.com>
2018-01-08 01:26:50 -0700

    Add regex on url variable

--- slate/routes.py
+++ slate/routes.py
@@ -8,7 +8,7 @@ def setup_routes(app: web.Application):
     app.router.add_get('/health/', health, name='health')
     app.router.add_get('/history/', history, name='history')
 
-    app.router.add_get('/channel/{channel_id}/', channel, name='channel')
+    app.router.add_get('/channel/{channel_id:[A-Z0-9]+}/', channel, name='channel')
 
     app.router.add_get('/oauth/redirect/', oauth_redirect, name='oauth-redirect')
     app.router.add_get('/oauth/verify/', oauth_verify, name='oauth-verify')
